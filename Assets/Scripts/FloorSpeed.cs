31d2a4030ebe5ac621b656835038e9b31252d071
makazemafuu <102394629+makazemafuu@users.noreply.github.com>
2022-03-26 22:40:10 +0100

    Script FloorSpeed
    Made it so the floor 'moves' for the game (surf-like game). Need to learn how to do triggers & other scripts I need for the capsule !

--- Assets/Scripts/FloorSpeed.cs
+++ Assets/Scripts/FloorSpeed.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class FloorSpeed : MonoBehaviour
 {
+
+    [SerializeField] private float speed;
+
+    private float timeSpan = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+    timeSpan += Time.deltaTime * 2;
+    Debug.Log(transform.forward * (speed / 10) * timeSpan);
+    transform.position -= transform.forward * (speed / 10) * timeSpan;
     }
 }
